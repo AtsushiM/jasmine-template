a894bb9b55007ff87e987c2a67db8a23a7b4712c
Mizoue Atsushi <asionfb@gmail.com>
2012-11-05 19:11:47 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,70 @@
-// テストしたいクラス名を設定
-describe('XXXXX クラスは', function() {
-    //テストしたい変数名
-    var a = null,
-        b = null;
+/* Class: "%CLASS_PATH%" */
+describe('%CLASS%は', function() {
+    var %CLASS_LOW%;
+
     beforeEach(function() {
-        //初期値
-        a = 'test';
-        b = 'test';
+        // init
+        %CLASS_LOW% = new Global.%CLASS%();
     });
     afterEach(function() {
-        //テスト実行後の処理(DOM操作をした場合などに初期化するために使用するなど)
+        // clear
     });
 
-    //テストのテンプレート
-    /*
-    it('XXXX は XXXXX で XXXXX', function() {
-        //非同期処理
+    // it('singletonである', function() {
+    //     var %CLASS_LOW%1 = new %CLASS%(),
+    //         %CLASS_LOW%2 = new %CLASS%();
+
+    //     expect(%CLASS_LOW%1).toBe(%CLASS_LOW%2);
+    // });
+
+    // it('XXXの機能をすべて持つ', function() {
+    //     var org = new XXX(),
+    //         %CLASS_LOW% = new %CLASS%();
+
+    //     for (var i in org) {
+    //         if (org.hasOwnProperty(i)) {
+    //             expect(%CLASS_LOW%[i]).toBeDefined();
+    //         }
+    //     }
+    // });
+
+    it('zzzである', function() {
+        expect(0).toEqual(1);
+    });
+});
+/*
+describe('XXXは', function() {
+    it('XXX', function() {
+        //spyOn
+        //呼び出しに対してargumentsを返却
+        spyOn(obj, 'method').andReturn(arguments);
+        //呼び出しに対して例外を発生させる
+        spyOn(obj, 'method').andThrow(exception);
+        //呼び出しに対して代わりの関数を実行させる
+        spyOn(obj, 'method').andCallFake(function);
+        //呼び出しに対してそのまま本来のメソッドを呼び出す
+        spyOn(obj, 'method').andCallThrough(function);
+
+        // spy後
+        // 呼び出しが行われたか
+        expect(obj, method).toHaveBeenCalled();
+        // 呼び出しがargumentsを伴って呼び出されたか
+        expect(obj, method).toHaveBeenCalledWith(arguments);
+
+        //呼び出し回数
+        obj.method.callCount;
+        //直近の読み出し時の引数
+        obj.mostRecentCall.args
+        // i番目の呼び出し時
+        obj.argsForCall[i]
+
         runs(function() {
-            //add code.
+            //処理
         });
+        // １秒待ち
         waits(1000);
         runs(function() {
-            //add code.
+            //処理
         });
 
         //aがbと同じである
@@ -64,5 +107,5 @@ describe('XXXXX クラスは', function() {
         //a（function）が例外をスローしない
         expect(a).not.toThrow(e);
     });
-    */
 });
+*/
